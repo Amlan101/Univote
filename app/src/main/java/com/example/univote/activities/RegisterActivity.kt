537d94da13c0818176fb54babd7497de8309c4ae
Amlan <amlan.sahooricky@gmail.com>
2024-11-08 17:01:21 +0530

    Implemented a basic registration page

--- app/src/main/java/com/example/univote/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/univote/activities/RegisterActivity.kt
@@ -6,11 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.univote.R
+import com.example.univote.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.registerBtn.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // TODO: Add API call to register the user
+                Snackbar.make(binding.registerBtn, "Registration functionality to be implemented", Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(binding.registerBtn, "Please fill all fields", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
