b4144d2b4c5d54d9e738fcedf03f190f9c2ada52
Amlan <amlan.sahooricky@gmail.com>
2024-11-08 17:07:04 +0530

    Created a retrofit instance

---
+++ app/src/main/java/com/example/univote/network/RetrofitInstance.kt
@@ -0,0 +1,26 @@
+package com.example.univote.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    // TODO: Add the Base URL of the Golang API
+    private const val BASE_URL = ""
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
